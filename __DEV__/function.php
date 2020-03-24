586769aeb925063a7280bec69078d21ffd089f39
mak <makindosx@gmail.com>
2020-03-24 20:02:49 +0200

    Create function.php

---
+++ __DEV__/function.php
@@ -0,0 +1,30 @@
+<?php
+
+#
+# Copyright (c) 2018 Barchampas Gerasimos <makindosx@gmail.com>
+# spy-microphone is a program for steal voice in gnu/linux OS.
+#
+# spy-microphone is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+#
+# spy-microphone is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License, version 3,
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+
+function input($data)
+       {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+        return $data;
+           }
+
+?>
